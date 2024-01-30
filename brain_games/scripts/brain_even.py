d2576ef890de45be2dec9122208aba4f6fdcbeea
Zorin Alexey <zorinap@LAPTOP-2R6OUKU8>
2024-01-30 22:33:29 +0300

    5.2 stage

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -11,21 +11,31 @@ def main_even():
         name = input()
     print(f'Hello, {name}!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    number_1 = random.randint(1,100)
-    print('Question:',number_1)
-    answer = ''
-    print('Your answer:', end='')
-    answer = input()
-    if number_1 % 2 == 0 and answer == 'yes':
-        print('Correct!')
-    if number_1 % 2 != 0 and answer == 'no':
-        print('Correct!')
-    if number_1 % 2 == 0 and answer != 'yes':
-        print(f"'{answer}' is wrong answer ;(. Correct answer was 'yes'")
-        print(f"Let's try again, {name}!")
-    if number_1 % 2 != 0 and answer != 'no':
-        print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'")
-        print(f"Let's try again, {name}!")
+    i = 1
+    while i <= 3:
+        number_1 = random.randint(1,100)
+        print('Question:',number_1)
+        answer = ''
+        print('Your answer:', end='')
+        answer = input()
+        if number_1 % 2 == 0 and answer == 'yes':
+            print('Correct!')
+            i = i + 1
+            if i > 3:
+                print(f'Congratulations, {name}!')
+        if number_1 % 2 != 0 and answer == 'no':
+            print('Correct!')
+            i = i + 1
+            if i > 3:
+                print(f'Congratulations, {name}!')
+        if number_1 % 2 == 0 and answer != 'yes':
+            print(f"'{answer}' is wrong answer ;(. Correct answer was 'yes'")
+            print(f"Let's try again, {name}!")
+            exit()
+        if number_1 % 2 != 0 and answer != 'no':
+            print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'")
+            print(f"Let's try again, {name}!")
+            exit()
 
 if __name__ == '__main_even__':
     main_even()
